0c20366a1fad0c5aaab934acb8cf1ceae7e5f7d7
aayushmaanhooda <aayushmaan162@gmail.com>
2026-02-04 15:12:15 +1100

    understanding pipelines

--- bookCrawltwo/bookCrawltwo/items.py
+++ bookCrawltwo/bookCrawltwo/items.py
@@ -2,7 +2,23 @@
 from scrapy.item import Item, Field
 
 class BookcrawltwoItem(Item):
+    pass
+
+def seralize_price(value):
+    return f"${str(value)}"
+
+class BookItem(Item):
+    url = Field()
     title = Field()
+    upc = Field()
+    product_type = Field()
+    # price_excl_tax = Field(serializer = seralize_price)
+    price_excl_tax = Field()
+    price_incl_tax = Field()
+    tax = Field()
+    availability = Field()
+    num_reviews = Field()
+    stars = Field()
     category = Field()
     description = Field()
     price = Field()
